d9967a16b5c67501a9eed29ac56ae34db8ae8a60
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-03-30 01:55:52 +0100

    Ficha Empezado (Que malo es el insomnio)

--- src/analisis/ejercicio2/Ficha.java
+++ src/analisis/ejercicio2/Ficha.java
@@ -2,4 +2,35 @@ package analisis.ejercicio2;
 
 public class Ficha {
 
+	private int id;
+
+	private String titulo;
+
+	Ficha(int id, String titulo) {
+
+		if (id > 0) {
+			this.id = id;
+		}
+
+		if (titulo != null && !titulo.isBlank()) {
+			this.titulo = titulo;
+		}
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	@Override
+	public String toString() {
+
+		return "ID: " + id + ", Titulo: " + titulo;
+
+	}
+
 }
