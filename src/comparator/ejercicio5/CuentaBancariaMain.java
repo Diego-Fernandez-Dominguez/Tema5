fcba6e6569132be0a1e1eb154144f8a13d18d5e1
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-04-07 19:42:07 +0200

    Ejercicio 5 Comparator Acabado

--- src/comparator/ejercicio5/CuentaBancariaMain.java
+++ src/comparator/ejercicio5/CuentaBancariaMain.java
@@ -1,19 +1,55 @@
 package comparator.ejercicio5;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class CuentaBancariaMain {
 	public static void main(String[] args) {
-		
-		Titular tit1 = new Titular("12345678A", "Josebas", "Perez Ruiz", 123456789);
-		Titular tit2 = new Titular("23456789B", "Euseboi", "Moreno Diaz ", 234567891);
-		
-		CuentaCorriente cc1 = new CuentaCorriente(1234, tit1, 0.21);
 
-		/*Comparator<CuentaCorriente> numCuentaAsc = (a,b) ->{
-			
-		};*/
-		
+		CuentaBancaria[] cuentas = new CuentaBancaria[2];
+		Titular[] titu = new Titular[3];
+
+		Titular tit1 = new Titular("12345678A", "A", "Z", 123456789);
+		Titular tit2 = new Titular("23456789B", "B", "Z", 234567891);
+		Titular tit3 = new Titular("23456789C", "B", "A", 234567891);
+
+		CuentaBancaria cb1 = new CuentaBancaria(1234, tit1);
+		CuentaBancaria cb2 = new CuentaBancaria(9876, tit2);
+
+		cb1.ingresar(56);
+		cb2.ingresar(3);
+
+		cuentas[0] = cb2;
+		cuentas[1] = cb1;
+
+		titu[0] = tit2;
+		titu[1] = tit1;
+		titu[2] = tit3;
+
+		Comparator<CuentaBancaria> numCuentaDsc = (a, b) -> {
+			return (int) (b.getSaldo() - a.getSaldo());
+		};
+
+		Arrays.sort(cuentas, numCuentaDsc);
+		System.out.println(Arrays.toString(cuentas));
+
+		Comparator<Titular> apellNombr = (a, b) -> {
+
+			int dev = 0;
+
+			if (a.getApellidos().equals(b.getApellidos())) {
+				dev = a.getNombre().compareTo(b.getNombre());
+			} else {
+				dev = a.getApellidos().compareTo(b.getApellidos());
+			}
+
+			return dev;
+
+		};
+
+		Arrays.sort(titu, apellNombr);
+		System.out.println(Arrays.toString(titu));
+
 	}
 
 }
--- src/comparator/ejercicio5/Titular.java
+++ src/comparator/ejercicio5/Titular.java
@@ -27,6 +27,38 @@ public class Titular implements Comparable<Titular> {
 
 	}
 
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
 	@Override
 	public String toString() {
 		return "DNI: " + dni + ", Nombre: " + nombre + ", Apellidos: " + apellidos + ", Telefono: " + telefono;
