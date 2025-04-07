11a74f714801a781a40b4610b44e62139d1db215
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-04-07 12:38:16 +0200

    Inicio ejercicio 5 boletin comparator

--- src/comparator/ejercicio4/FichaMain.java
+++ src/comparator/ejercicio4/FichaMain.java
@@ -1,26 +1,39 @@
 package comparator.ejercicio4;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class FichaMain {
 	public static void main(String[] args) {
-		
-		Ficha[] fichas=new Ficha[5];
-		
-		Comparator<Ficha> idAsc= (a,b)->{
-			return a.getId()-b.getId();
-		};
-		
-		Ficha f1 = new Libro(1, "Libro Hola", "Autor", "Titular");
-		Ficha f2 = new Revista(2, "Revista Hola", 12, 2024);
-		Ficha f3 = new DVD(3, "DVD Hola", "Director", 2016, "Documental");
-		Ficha f4 = new DVD(4, "DVD wf", "Director", 2016, "Documental");
-		Ficha f5 = new DVD(5, "DVD tjh", "Director", 2016, "Documental");
 
-		
-		
-		
-		
+		Ficha[] fichas = new Ficha[5];
+
+		Comparator<Ficha> idAsc = (a, b) -> {
+			return a.getId() - b.getId();
+		};
+
+		Comparator<Ficha> ordAlf = (a, b) -> {
+			return a.getTitulo().compareTo(b.getTitulo());
+		};
+
+		Ficha f1 = new Libro(1, "e", "Autor", "Titular");
+		Ficha f2 = new Revista(2, "d", 12, 2024);
+		Ficha f3 = new DVD(3, "c", "Director", 2016, "Documental");
+		Ficha f4 = new DVD(4, "b", "Director", 2016, "Documental");
+		Ficha f5 = new DVD(5, "a", "Director", 2016, "Documental");
+
+		fichas[0] = f1;
+		fichas[1] = f2;
+		fichas[2] = f3;
+		fichas[3] = f4;
+		fichas[4] = f5;
+
+		Arrays.sort(fichas, idAsc);
+		System.out.println(Arrays.toString(fichas));
+
+		Arrays.sort(fichas, ordAlf);
+		System.out.println(Arrays.toString(fichas));
+
 	}
 
 }
