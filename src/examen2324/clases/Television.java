0de790395fb0a43cd4af91c797ca68b930d90e52
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-04-09 18:50:34 +0200

    Mejora television

--- src/examen2324/clases/Television.java
+++ src/examen2324/clases/Television.java
@@ -4,7 +4,8 @@ import examen2324.exceptions.ModeloException;
 import excepciones.ejercicio3.PrecioException;
 
 /**
- * Clase que va a guardar los atributos mas importantes de un mando de television
+ * Clase que va a guardar los atributos mas importantes de un mando de
+ * television
  */
 public class Television extends Mando implements Volumen {
 
@@ -12,7 +13,7 @@ public class Television extends Mando implements Volumen {
 	 * Volumen del mando
 	 */
 	private int volumen = 0;
-	
+
 	/**
 	 * Canal del mando
 	 */
@@ -24,15 +25,18 @@ public class Television extends Mando implements Volumen {
 	 * @param anchura Anchura del mando
 	 * @param altura  Altura del mando
 	 * @param precio  Precio del mando
-	 * @throws ModeloException Excepcion que salta cuando el modelo es nulo o en blanco
+	 * @throws ModeloException Excepcion que salta cuando el modelo es nulo o en
+	 *                         blanco
 	 * @throws PrecioException Excepcion que salta cuando el precio es negativo
 	 */
-	public Television(String modelo, double anchura, double altura, double precio) throws ModeloException, PrecioException {
+	public Television(String modelo, double anchura, double altura, double precio)
+			throws ModeloException, PrecioException {
 		super(modelo, anchura, altura, precio);
 	}
 
 	/**
 	 * Metodo que devuelve el volumen del mando
+	 * 
 	 * @return Devuelve el volumen del mando
 	 */
 	public int getVolumen() {
@@ -41,6 +45,7 @@ public class Television extends Mando implements Volumen {
 
 	/**
 	 * Metodo que devuelve el canal del mando
+	 * 
 	 * @return Devuelve el canal del mando
 	 */
 	public int getCanal() {
@@ -49,15 +54,50 @@ public class Television extends Mando implements Volumen {
 
 	/**
 	 * Metodo que cambia el canal de television
+	 * 
 	 * @param canal El canal por el que cambiar
 	 */
 	public void setCanal(int canal) {
 		this.canal = canal;
 	}
 
+	/**
+	 * Metodo para subir el canal del mando de 1 en 1
+	 * 
+	 * @return Devuelve true si se ha podido y false si no ha podido
+	 */
+	public boolean subirCanal() {
+
+		boolean sePudo = false;
+
+		if (canal + 1 <= 100) {
+			sePudo = true;
+			canal++;
+		}
+
+		return sePudo;
+	}
+
+	/**
+	 * Metodo para bajar el canal del mando de 1 en 1
+	 * 
+	 * @return Devuelve true si se ha podido y false si no ha podido
+	 */
+	public boolean bajarCanal() {
+		boolean sePudo = false;
+
+		if (canal - 1 >= 1) {
+			sePudo = true;
+			canal--;
+		}
+
+		return sePudo;
+	}
+
 	@Override
 	/**
 	 * Metodo para subir el volumen del mando
+	 * 
 	 * @return Devuelve true si se ha podido y false si no ha podido
 	 */
 	public boolean subirVolumen() {
@@ -75,6 +115,7 @@ public class Television extends Mando implements Volumen {
 	@Override
 	/**
 	 * Metodo para bajar el volumen del mando
+	 * 
 	 * @return Devuelve true si se ha podido y false si no ha podido
 	 */
 	public boolean bajarVolumen() {
